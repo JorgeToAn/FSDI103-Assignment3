454c0df70988a72f320c791b27ef17830d157a96
JorgeToAn <106716983+JorgeToAn@users.noreply.github.com>
2022-06-26 15:28:29 -0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,14 @@
+function calculator(){
+    console.log(`Calculating...`);
+    //get the values from the prompt
+    let num1 = prompt("Enter the first number");
+    let num2 = prompt("Enter the second number");
+
+    //do the four operations
+    let sum = num1+num2;
+    let minus = num1-num2;
+    let multiplication = num1;
+    let division = num1/num2;
+
+    //display the results in the results zone
+}
