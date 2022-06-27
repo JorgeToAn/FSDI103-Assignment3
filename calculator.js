077dddaf6d38d4584ce8d84b933e7601c2ec35d1
JorgeToAn <jorgetosta.f@gmail.com>
2022-06-26 17:21:24 -0700

    Completed assignment

--- calculator.js
+++ calculator.js
@@ -1,14 +1,29 @@
 function calculator(){
     console.log(`Calculating...`);
     //get the values from the prompt
-    let num1 = prompt("Enter the first number");
-    let num2 = prompt("Enter the second number");
+    let num1 = Number(prompt("Enter the first number"));
+    let num2 = Number(prompt("Enter the second number"));
 
     //do the four operations
     let sum = num1+num2;
-    let minus = num1-num2;
-    let multiplication = num1;
+    let substraction = num1-num2;
+    let multiplication = num1*num2;
     let division = num1/num2;
 
     //display the results in the results zone
+    document.getElementById("results").innerHTML=
+        `<table>
+            <tr>
+                <th>Sum <i class="fa-solid fa-plus"></i></th>
+                <th>Substraction <i class="fa-solid fa-minus"></i></th>
+                <th>Multiplication <i class="fa-solid fa-xmark"></i></th>
+                <th>Division <i class="fa-solid fa-divide"></i></th>
+            </tr>
+            <tr>
+                <td>${sum}</td>
+                <td>${substraction}</td>
+                <td>${multiplication}</td>
+                <td>${division}</td>
+            </tr>
+        </table>`;
 }
